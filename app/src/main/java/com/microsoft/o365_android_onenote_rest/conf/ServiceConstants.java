f854f359442407561ba4a55d20eed5ef101a98bb
Stefanie Hansen <stefuhnee@gmail.com>
2017-05-17 13:37:29 -0700

    Reverted to placeholder configuration values

--- app/src/main/java/com/microsoft/o365_android_onenote_rest/conf/ServiceConstants.java
+++ app/src/main/java/com/microsoft/o365_android_onenote_rest/conf/ServiceConstants.java
@@ -3,11 +3,11 @@ package com.microsoft.o365_android_onenote_rest.conf;
 public class ServiceConstants {
 
     // MSA
-    public static final String MSA_CLIENT_ID = "3c838cf7-9a39-4ca9-8d6a-bf483d9427de";
+    public static final String MSA_CLIENT_ID = "<Your REDIRECT URI Here>";
     public static final String ONENOTE_API = "https://graph.microsoft.com";
     // Org
     public static final String AUTHENTICATION_RESOURCE_ID = "https://graph.microsoft.com";
     public static final String AUTHORITY_URL = "https://login.microsoftonline.com/common";
-    public static final String REDIRECT_URI = "http://localhost/OneNoteRESTExplorer";
-    public static final String CLIENT_ID = "8f73da4f-a1b3-43ea-884f-8891182d6296";
+    public static final String REDIRECT_URI = "<Your REDIRECT URI Here>";
+    public static final String CLIENT_ID = "<Your CLIENT ID Here>";
 }
